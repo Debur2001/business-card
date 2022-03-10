fc49d2cf8b05d12c59f366b67bec416626353cce
Debur <vini.bernert@yahoo.com.br>
2022-03-09 23:52:58 -0300

    inclusao das dependencias

---
+++ app/src/main/java/com/example/businesscard/App.kt
@@ -0,0 +1,7 @@
+package com.example.businesscard
+
+import android.app.Application
+
+class App : Application() {
+
+}
--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.businesscard
+package com.example.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
