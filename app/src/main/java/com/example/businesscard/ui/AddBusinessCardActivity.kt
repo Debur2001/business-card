c198bebada62b6ea629b0c284b341afa6bc8f52a
Debur <vini.bernert@yahoo.com.br>
2022-03-10 23:34:34 -0300

    Finaliza configuracao do Room Database

--- app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,20 @@ package com.example.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.businesscard.App
+import com.example.businesscard.R
+import com.example.businesscard.data.BusinessCard
 import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +28,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.btnConfirma.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString(),
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_sucess, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
