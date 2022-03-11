4ea43eaab217a45eadbe4a04f30f65121a42c2a1
Debur <vini.bernert@yahoo.com.br>
2022-03-11 00:57:49 -0300

    Implementa geracao do bitmap do cartao e compartilhamento com outros apps

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.businesscard.App
 import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.util.Image
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
         }
     }
 
